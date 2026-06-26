c947561409915a2ecef2a25976e24d0c260f0888
deguzman20 <alejandrogacumadeguzman@gmail.com>
2026-06-26 18:54:05 +0800

    Added frontend validations and error for the prevention of expense future date

--- frontend/src/components/ExpenseForm.tsx
+++ frontend/src/components/ExpenseForm.tsx
@@ -44,6 +44,8 @@ export function ExpenseForm({
     label: category,
   }));
 
+  const date = new Date().toISOString().split("T")[0];
+
   return (
     <form onSubmit={handleSubmit} style={formStyle}>
       <TextField
@@ -87,6 +89,8 @@ export function ExpenseForm({
         error={errors.date}
         fullWidth
         required
+        max={date}
+        defaultValue={date}
       />
 
       <div style={buttonGroupStyle}>
